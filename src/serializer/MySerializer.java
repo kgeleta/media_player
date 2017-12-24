aa58ffbd1bbb634233f51db877df418f4f1c93f0
Krzysztof Geleta <homar425@gmail.com>
2017-12-24 13:59:46 +0100

    serialization

--- src/serializer/MySerializer.java
+++ src/serializer/MySerializer.java
@@ -1,6 +1,33 @@
 package serializer;
 
+import java.io.File;
+
 public class MySerializer
 {
+    public static String serializeToString(File[] files)
+    {
+        String serialized = "";
+        for (File file : files)
+        {
+            if (!file.isHidden())
+            {
+                if (file.isFile())
+                    serialized += ("<F>" + file.getName() + "</F>");
+                else if (file.isDirectory())
+                    serialized += ("<D>" + file.getName() + "</D>");
+            }
+        }
+        return serialized;
+    }
 
+    public static MyFile[] serializeToMyFile(String serialized)
+    {
+        return null;
+    }
+
+
+    public static void main(String[] args)
+    {
+        System.out.println(MySerializer.serializeToString(new File("\\").listFiles()));
+    }
 }
