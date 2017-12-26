a7e2e9ff0e9587e41ec8b6373401cab35689d4d7
Krzysztof Geleta <homar425@gmail.com>
2017-12-26 12:18:43 +0100

    GUI Style

--- src/gui/ServerApplication.java
+++ src/gui/ServerApplication.java
@@ -41,6 +41,17 @@ public class ServerApplication extends Application
         vBox.setAlignment(Pos.CENTER);
         vBox.setMinSize(300,275);
 
+        //Set style:
+        start.setStyle("-fx-background-color:" +
+                "        linear-gradient(#f0ff35, #a9ff00)," +
+                "        radial-gradient(center 50% -40%, radius 200%, #b8ee36 45%, #80c800 50%);" +
+                "    -fx-background-radius: 6, 5;" +
+                "    -fx-background-insets: 0, 1;\n" +
+                "    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.4) , 5, 0.0 , 0 , 1 );" +
+                "    -fx-text-fill: #395306;-fx-font-size: 30px;");
+        vBox.setStyle("-fx-background-color: #373737");
+        statusLabel.setStyle("-fx-text-fill: #b8ee36;-fx-font-size: 45px;");
+
 
         start.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -55,8 +66,10 @@ public class ServerApplication extends Application
                 }
             }
         });
-        vBox.getChildren().add(start);
+
         vBox.getChildren().add(statusLabel);
+        vBox.getChildren().add(start);
+
         statusLabel.setVisible(false);
 
         Scene scene = new Scene(vBox);
